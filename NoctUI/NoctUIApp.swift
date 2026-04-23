bead6254798cad00060a120400aa557f0e532d2e
dev_alvin <alvinmatthew12@gmail.com>
2026-04-24 00:00:13 +0700

    Initial commit

---
+++ NoctUI/NoctUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoctUIApp.swift
+//  NoctUI
+//
+//  Created by BlackCurrant
+//
+
+import SwiftUI
+
+@main
+struct NoctUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
